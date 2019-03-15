d254a864c02c4d290d4cebdd18059c31f95110e2
amirisback <faisalamircs@gmail.com>
2019-03-15 10:14:54 +0700

    Initial commit

---
+++ app/src/main/java/com/frogobox/footballmvvm/view/MainActivity.java
@@ -0,0 +1,15 @@
+package com.frogobox.footballmvvm.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.frogobox.footballmvvm.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
